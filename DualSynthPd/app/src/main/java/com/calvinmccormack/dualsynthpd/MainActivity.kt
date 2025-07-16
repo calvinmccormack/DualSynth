e78d07cebeb8ef2742a2e20ef0d9039d121581cd
Calvin McCormack <calvinmccormackmusic@gmail.com>
2025-07-16 20:03:55 +0530

    Started Kotlin+Pd Project, builds and plays a440

---
+++ DualSynthPd/app/src/main/java/com/calvinmccormack/dualsynthpd/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.calvinmccormack.dualsynthpd
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.calvinmccormack.dualsynthpd.ui.theme.DualSynthPdTheme
+
+import java.io.File
+import org.puredata.core.PdBase
+import org.puredata.android.io.PdAudio
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Copy the .pd patch from assets to internal storage
+        val patchFile = File(filesDir, "a440.pd")
+        assets.open("pd-patches/a440.pd").use { input ->
+            patchFile.outputStream().use { output ->
+                input.copyTo(output)
+            }
+        }
+
+        // Initialize audio
+        PdAudio.initAudio(44100, 0, 2, 8, true)
+
+        // Open patch
+        PdBase.openPatch(patchFile)
+
+        // Start audio
+        PdAudio.startAudio(this)
+
+        enableEdgeToEdge()
+        setContent {
+            DualSynthPdTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "PD is running",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        PdAudio.stopAudio()
+        PdBase.release()
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DualSynthPdTheme {
+        Greeting("Android")
+    }
+}
